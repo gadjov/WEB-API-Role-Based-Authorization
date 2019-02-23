daf31500ee076d06bc00ce0bb665d48898aac9c6
gadjov <vladko_gadjov@abv.bg>
2019-02-23 21:11:26 +0200

    WEB-API-Role-Based-Authorization

---
+++ RoleBasedAuthorization/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace RoleBasedAuthorization.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ RoleBasedAuthorization/Interfaces/IUSerService.cs
@@ -0,0 +1,12 @@
+﻿using RoleBasedAuthorization.Models;
+using System.Collections.Generic;
+
+namespace RoleBasedAuthorization.Interfaces
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+}
